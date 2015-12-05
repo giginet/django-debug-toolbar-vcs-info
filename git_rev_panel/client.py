6e8dbc86afba21a4a575af78bfc1f8690767b8bb
giginet <giginet.net@gmail.com>
2015-12-05 22:53:18 +0900

    Add short hash on panel

--- git_rev_panel/client.py
+++ git_rev_panel/client.py
@@ -1,6 +1,26 @@
-class GitClient(object):
-    def __init__(self):
-        pass
+import os
+import subprocess
+from subprocess import CalledProcessError
 
+
+class GitClient(object):
     def is_gitdir(self):
-        pass
+        try:
+            is_work_dir = self._execute_git('rev-parse', '--is-inside-work-tree')
+            return is_work_dir.startswith('true')
+        except CalledProcessError:
+            return False
+
+    def get_cwd(self):
+        return os.getcwd()
+
+    def get_hash(self):
+        return self._execute_git('rev-parse', 'HEAD')
+
+    def get_short_hash(self):
+        return self._execute_git('rev-parse', '--short', 'HEAD')
+
+    def _execute_git(self, main, *options):
+        commands = ['git', main] + list(options)
+        raw_data = subprocess.check_output(commands)
+        return raw_data.decode()
--- git_rev_panel/panels.py
+++ git_rev_panel/panels.py
@@ -1,8 +1,18 @@
 from debug_toolbar.panels import Panel
+from .client import GitClient
 
 
 class GitRevisionPanel(Panel):
     title = 'Revision'
 
+    def __init__(self, *args, **kwargs):
+        self.client = GitClient()
+        super().__init__(*args, **kwargs)
+
     def has_content(self):
         return False
+
+    def nav_subtitle(self):
+        if self.client.is_gitdir():
+            return self.client.get_short_hash()
+        return 'git repository is not detected'

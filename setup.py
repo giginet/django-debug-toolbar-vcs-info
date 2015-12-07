4925e133b89271d67db0906d9850dd19072592d9
giginet <giginet.net@gmail.com>
2015-12-07 23:42:14 +0900

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,74 @@
+# coding=utf-8
+import sys
 from setuptools import setup
 
+NAME = 'django-debug-toolbar-vcs-info'
+VERSION = '1.0.0'
+
+
+def read(filename):
+    import os
+    BASE_DIR = os.path.dirname(__file__)
+    filename = os.path.join(BASE_DIR, filename)
+    with open(filename, 'r') as fi:
+        return fi.read()
+
+
+def readlist(filename):
+    rows = read(filename).split("\n")
+    rows = [x.strip() for x in rows if x.strip()]
+    return list(rows)
+
+# if we are running on python 3, enable 2to3 and
+# let it use the custom fixers from the custom_fixers
+# package.
+extra = {}
+if sys.version_info >= (3, 0):
+    extra.update(
+        use_2to3=True,
+    )
+
 setup(
-    name = "django-debug-toolbar-vcs-info",
-    version = "0.0.1",
-    packages=['vcs_info_panel'],
+    name=NAME,
+    version=VERSION,
+    description=('A Django Debug Toolbar panel to show VCS info'),
+    long_description = read('README.rst'),
+    classifiers = (
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    keywords = 'django django-debug-toolbar git vcs info revision hash panel',
+    author = 'giginet',
+    author_email = 'giginet.net@gmail.com',
+    url = 'https://github.com/giginet/%s' % NAME,
+    download_url = 'https://github.com/giginet/%s/tarball/master' % NAME,
+    license = 'MIT',
+    packages = ('vcs_info_panel',),
+    package_dir = {'': 'vcs_info_panel'},
+    include_package_data = True,
+    package_data = {
+        '': ['README.rst',
+             'requirements.txt',
+             'requirements-test.txt'],
+    },
+    zip_safe=True,
+    install_requires=readlist('requirements.txt'),
+    test_suite='runtests.run_tests',
+    tests_require=readlist('requirements-test.txt'),
+    **extra
 )
